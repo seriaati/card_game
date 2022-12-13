46be28b9f24901eabcbe87f1aed561bd1cd6a83e
seriaati <seria.ati@gmail.com>
2022-12-13 17:57:34 +0800

    Update enums.py

--- enums.py
+++ enums.py
@@ -2,24 +2,30 @@ from enum import Enum
 
 
 class WeaponType(Enum):
-    SWORD = 1
-    CATALYST = 2
-    BOW = 3
-    CLAYMORE = 4
-    POLEARM = 5
-    OTHER = 6
+    SWORD = "Sword"
+    CATALYST = "Catalyst"
+    BOW = "Bow"
+    CLAYMORE = "Claymore"
+    POLEARM = "Polearm"
+    OTHER = "Other"
 
 class Element(Enum):
-    PYRO = 1
-    HYDRO = 2
-    ELECTRO = 3
-    ANEMO = 4
-    GEO = 5
-    CRYO = 6
-    DENDRO = 7
-    ALL = 8
-    NONE = 9
+    PYRO = "Pyro"
+    HYDRO = "Hydro"
+    ELECTRO = "Electro"
+    ANEMO = "Anemo"
+    GEO = "Geo"
+    CRYO = "Cryo"
+    DENDRO = "Dendro"
+    ALL = "All"
+    NONE = "None"
 
-class Team(Enum):
-    MONSTER = 1
-    CHARACTER = 2
+class Faction(Enum):
+    MONDSTAT = "Mondstat"
+    LIYUE = "Liyue"
+    INAZUMA = "Inazuma"
+    SUMERU = "Sumeru"
+    FATUI = "Fatui"
+    HILICHURL = "Hilichurl"
+    MONSTER = "Monster"
+    OTHER = "Other"
