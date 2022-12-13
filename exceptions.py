21bfd71a399300c6133142e2d724712673e9f48c
seriaati <seria.ati@gmail.com>
2022-12-13 19:03:52 +0800

    Update exceptions

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,7 @@
-class NotThirtyCard(Exception):
+class TooManyCharacters(Exception):
     def __init__(self):
-        self.message = "Deck must have 30 cards"
+        self.message = "A player can't have more than 3 characters"
+
+class TooManyCards(Exception):
+    def __init__(self):
+        self.message = "A deck can't have more than 30 cards"
