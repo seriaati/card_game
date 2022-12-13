b6740863e1d724e5f92be7b07166a3347ebb0c46
seriaati <seria.ati@gmail.com>
2022-12-13 19:03:07 +0800

    Update deck.py

--- classes/deck.py
+++ classes/deck.py
@@ -1,11 +1,17 @@
 from typing import List
 
-from classes.main_cards import Card
-from exceptions import NotThirtyCard
+from classes.card import ActionCard, CharacterCard
+from exceptions import TooManyCards, TooManyCharacters
 
 
 class Deck(object):
-    def __init__(self, cards: List[Card]):
-        self.cards = cards
-        if len(cards) != 30:
-            raise NotThirtyCard
+    def __init__(
+        self, character_cards: List[CharacterCard], action_cards: List[ActionCard]
+    ):
+        self.cards: List[ActionCard | CharacterCard] = character_cards + action_cards
+        self.character_cards = character_cards
+        self.action_cards = action_cards
+        if len(self.cards) != 30:
+            raise TooManyCards
+        if len(self.character_cards) != 3:
+            raise TooManyCharacters
